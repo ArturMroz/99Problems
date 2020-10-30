4784f154f4ae42076c0a11f0312f3c4317617b11
Artur Mroz <frost47@gmail.com>
2020-10-30 20:17:19 +0000

    update 4 in F#

--- Fsharp/4.fsx
+++ Fsharp/4.fsx
@@ -1,14 +1,14 @@
 // Find the number of elements of a list.
 // https://wiki.haskell.org/99_questions/1_to_10
 
-
 let myLength = List.length
 
-let myLength' xs =
-    let rec myLenghtRec acc = function
-        | [] -> acc
-        | _ :: xs -> myLenghtRec (acc + 1) xs
+let rec myLength' = function
+    | [] -> 0
+    | _ :: xs -> myLength' xs + 1
 
-    myLenghtRec 0 xs
+let myLength'' xs =
+    xs |> List.fold (fun acc _ -> acc + 1)
 
 myLength [ 1; 2; 3 ]
+
