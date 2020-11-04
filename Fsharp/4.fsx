4561744ccc5db92e54cc1f7e77e400405b988573
Artur Mroz <frost47@gmail.com>
2020-11-04 21:44:21 +0000

    add one more way to 4 in F#

--- Fsharp/4.fsx
+++ Fsharp/4.fsx
@@ -8,7 +8,9 @@ let rec myLength' = function
     | _ :: xs -> myLength' xs + 1
 
 let myLength'' xs =
-    xs |> List.fold (fun acc _ -> acc + 1)
+    xs |> List.fold (fun acc _ -> acc + 1) 0
+
+let myLength''' xs =
+    xs |> List.map (fun _ -> 1) |> List.sum
 
 myLength [ 1; 2; 3 ]
-
